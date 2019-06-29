e22608d427ba56438413da1256e414effa11f07e
Armando Restrepo <armando.restrepo@gmail.com>
2019-06-29 13:40:57 -0500

    feat: start using randomusers api

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,70 +1,110 @@
 import React, { Component } from 'react';
-import {Platform, TouchableHighlight, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform,
+  TouchableHighlight,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator
+} from 'react-native';
+import { Pages } from '../settings/navigation';
+import { displayName as appName } from '../../app.json';
 
 const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-      'Double tap R on your keyboard to reload,\n' +
-      'Shake or press menu button for dev menu',
-  });
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
 class HomeView extends Component {
+  static navigationOptions = {
+    title: appName
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {  };
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+  }
+  componentDidMount() {
+    return fetch('https://randomuser.me/api/?results=500')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.results,
+        }, function () {
+
+        });
+
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    const { navigate } = this.props.navigation
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, padding: 20 }}>
+          <ActivityIndicator />
+        </View>
+      )
     }
-    
-    render() {
-      const { navigate } = this.props.navigation
-        return (
-          <View style={styles.container}>
-            <Text style={styles.welcome}>Hello world!!!</Text>
-            <Text style={styles.instructions}>To get started, edit App.js</Text>
-            <Text style={styles.instructions}>{instructions}</Text>
-            <TouchableHighlight
-          onPress={() => navigate("Detail")}
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => <Text>{item.email}</Text>}
+          keyExtractor={({ login }, index) => login.uuid}
+        />
+        <TouchableHighlight
+          onPress={() => navigate(Pages.Detail)}
           style={styles.button}>
           <Text
-            style={styles.buttonText}>Screen One </Text>
-        </TouchableHighlight> 
-          </View>
-        );
-      }
+            style={styles.buttonText}>Screen One</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',  
-      margin: 10,
-    },
-    instructions: {
-      textAlign: 'center',
-      color: '#333333',
-      marginBottom: 5,
-    },
-    button: {
-      alignSelf: 'stretch',
-      marginLeft: 50,
-      marginRight: 50,
-      borderRadius: 5,
-      height: 40,
-      backgroundColor: '#7567B1',
-      justifyContent: 'center'
-    },
-    buttonText: {
-        color: 'white',
-        alignSelf: 'center',
-        fontSize: 16
-    }
-  });
-  
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  button: {
+    alignSelf: 'stretch',
+    marginLeft: 50,
+    marginRight: 50,
+    borderRadius: 5,
+    height: 40,
+    backgroundColor: '#7567B1',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    alignSelf: 'center',
+    fontSize: 16
+  }
+});
+
 
 export default HomeView;
